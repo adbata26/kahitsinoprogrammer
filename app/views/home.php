8af374d0aaaea99efa566fee1fc0e93e3fe0dcd9
adbata26 <86640582+adbata26@users.noreply.github.com>
2024-02-13 22:02:28 +0800

    Add files via upload

---
+++ app/views/home.php
@@ -0,0 +1,189 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>KAHIT SINO PROGRAMMER</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+  <style>
+      .nav-link
+      {
+        color:#41758e;
+      }
+
+      /* PHONE */
+ @media (max-width: 767px) {
+    #first_title
+      {
+        font-size:30px;font-weight:700;color:#41758e;
+      }
+
+      #second_title
+      {
+        font-size:20px;font-weight:700;
+      }
+
+      #third_title 
+      {
+        color:#8E5A41;font-size:25px;
+      }
+}
+
+  /* TABLET */
+@media (min-width: 768px) and (max-width: 991px) 
+{
+    #first_title
+      {
+        font-size:30px;font-weight:700;color:#41758e;
+      }
+
+      #second_title
+      {
+        font-size:20px;font-weight:700;
+      }
+
+      #third_title 
+      {
+        color:#8E5A41;font-size:25px;
+      }
+}
+
+/* Laptop */
+@media (min-width: 992px)  
+{
+    #first_title
+      {
+        font-size:60px;font-weight:700;color:#41758e;
+      }
+
+      #second_title
+      {
+        font-size:35px;font-weight:700;
+      }
+
+      #third_title 
+      {
+        color:#8E5A41;font-size:50px;
+      }
+}
+
+hr.new5 {
+    border: 10px solid #8e5a41;
+    border-radius: 5px;
+    width: 50%;
+    margin-left: auto; 
+}
+      
+    </style>
+
+    
+    <!-- Custom styles for this template -->
+    <link href="headers.css" rel="stylesheet">
+  </head>
+  <body style="font-family: 'Roboto', sans-serif;">
+  
+
+    
+
+
+   
+  
+    <nav class="navbar navbar-expand-md navbar-light d-flex justify-content-center py-3" style="background-color:#41758e;">
+      <img id="imagenav" src="app/views/files/kk.png" style="width: 100px;border-radius: 50%;">  
+   
+   </nav>
+   
+   <nav class="navbar navbar-expand-lg navbar-light" style="background-color:#8E5A41;font-size:12px;">
+    <div class="container">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a style="font-size:16px;" class="nav-link text-light" href="#">HOME</a>
+                </li>
+
+                <li class="nav-item">
+                    <a style="font-size:16px;" class="nav-link text-light" href="#about">ABOUT</a>
+                </li>
+
+                <li class="nav-item">
+                    <a style="font-size:16px;" class="nav-link text-light" href="#">BLOGS</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+
+     <div class="container">
+      <div class="row">
+        <div class="col-md-6 p-5">
+            <div id="first_title">PROGRAMMING IS LIKE A BREAD,</div>
+            <div id="second_title" class="text-end">IT'S FOR 
+            <span id="third_title">EVERYONE!</span></div>
+            <hr class="new5">
+            <div style="font-size:18px;" class="text-justify">"No matter which field of work you want to go in, it is of great importance to learn at least one programming language."</div>
+            <div class="text-end">- Ram Ray</div>
+        </div>
+
+
+        <div class="col-md-6 text-center pt-5">
+            <img src="app/views/files/2206.jpg" height="350">
+            
+         </div>
+
+
+         <div id="about" class="mt-5 p-5">
+            <div style="font-size:30px;font-weight:700;">ABOUT ME</div>
+            <div  style="font-size:25px;font-weight:400;" class="mt-3">
+                I'm Allen, a coding enthusiast!
+            </div>
+            <div class="text-justify" style="font-size:25px;font-weight:300;">
+                I named my website Kahit Sino Programmer dahil naniniwala ako na ang programming ay hindi lang para sa mga graduate ng IT related courses! Bagkus, ito ay para sa lahat! Yes, as in lahat! Estudyante, nanay, kuya, ate, tatay at pwede rin kahit si lolo at lola, bakit hindi? 
+            </div>
+            <div class="text-justify mt-3" style="font-size:25px;font-weight:700;">
+               Kung ang programming ay para sa lahat, bakit hindi lahat ay natututo mag-code?
+            </div>
+            <div class="text-justify mt-3" style="font-size:25px;font-weight:300;">
+              It is because it's <strong>not easy to learn</strong>, especially at first! Kailangan ng consistency sa pag-aaral para matuto mag-code. Unfortunately, not everyone has the priviledge to study programming because of a lot of factors that involve in our daily lives! 
+            </div>
+            <div class="text-justify mt-3" style="font-size:25px;font-weight:300;">
+             Pero isang factor kung bakit nakakawalang gana mag-aral ng programming ay dahil mahirap humanap nang libreng organize na tutorial! Most free courses available in Youtube are confusing because it's not clear to students kung ano ba ang sunod-sunod at saysay ng mga tutorials na pinapanood nila. 
+            </div>
+            <div class="text-justify mt-3" style="font-size:25px;font-weight:300;">
+             Another factor is that bootcamps and trainings cause pretty huge amount of money that most of us cannot afford because, hello?! We are in a third world country which means, we have a third world economy. That's why sobrang swerte mo na kung may budget ka to attend trainings! 
+            </div>
+
+            <div class="text-justify mt-3" style="font-size:25px;font-weight:300;">
+                That's why I'm trying to help promote programming by doing something that somehow, could help people overcome those two factors that I've mentioned. From time to time, I give tutorials on programming which I mostly announced in here and my Facebook page. 
+            </div>
+
+
+            <div class="text-justify mt-3" style="font-size:25px;font-weight:300;">
+                However, I will admit na ngayon pa lang, sinasabi ko na na hindi ako super expert and I still lack a lot of knowledge in the ocean wide of programming challenges. But it doesn't mean that I can't teach anything! In fact, what motivates me is the feedbacks I've received from my previous students. 
+            </div>
+
+            <div class="text-justify mt-3" style="font-size:25px;font-weight:300;">
+              So if you want to join the community that I'm building and read my postings that I'm sharing, you could like my page by clicking <a href="https://www.facebook.com/kahitsinoprogrammer" >here</a>
+            </div>
+
+         </div>
+      
+
+      </div> 
+
+
+
+   
+
+  </body>
+</html>
+
