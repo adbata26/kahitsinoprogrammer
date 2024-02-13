c67da7a42bef4e533fe8dca66b5e36810dc35020
adbata26 <86640582+adbata26@users.noreply.github.com>
2024-02-13 22:00:45 +0800

    Update App.php

--- app/App.php
+++ app/App.php
@@ -1,1 +1,65 @@
 
+<?php 
+class App
+{   
+
+    private function splitURL()
+	{
+		$url = $_SERVER['REQUEST_URI'];
+		$url = trim($url, '/'); 
+		$parts = explode('/', $url);
+
+		$controllername = 'home';
+	
+	
+		if(!empty($parts[0]))
+		{
+			$controllername = $parts[0];
+		}
+		return ucfirst($controllername);	
+	}
+
+    function loadController()
+    {
+		 
+		$thepage = $this->splitURL();
+
+		
+		$controllerFilePath = 'app/views/' . $thepage . '.php';
+		if (file_exists($controllerFilePath)) 
+		{
+            $filename = "app/views/" . $thepage . ".php";
+            if (file_exists($filename)) 
+            {
+                $filename = "app/views/".$thepage.".php";
+                if(file_exists($filename))
+                {
+                    require $filename;
+                }
+                
+                else
+                {
+        
+                    $filename = "app/views/_404.php";
+                    require $filename;
+                }
+            } 
+            else 
+            {            
+                $filename = "app/views/_404.php";
+                require $filename;    
+            }
+		}
+	    else 
+		{
+            $filename = "app/views/_404.php";
+            require $filename;
+		}
+
+
+		
+	
+
+    }
+
+}
